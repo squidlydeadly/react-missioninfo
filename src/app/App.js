59c97a04ec337aee0daf9396843a6a4169cde8d0
Vincent PORTA-SCARTA <vincent.porta@outlook.fr>
2018-06-25 23:29:33 -0400

    Remove build files

--- src/app/App.js
+++ src/app/App.js
@@ -7,7 +7,7 @@ import {BreadcrumbsProvider} from "react-breadcrumbs-dynamic";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename='/react-missioninfo/'>
         <BreadcrumbsProvider>
         <div>
           <Header/>
